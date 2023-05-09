e6ee09a186601d5ab9a10e212364e435208ed131
刘志伟 <2055572879@qq.com>
2023-05-09 13:32:59 +0800

    feat(custom): 更改gitee地址

--- packages/cli/src/util/contance.ts
+++ packages/cli/src/util/contance.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_TEMPLATE_SRC = 'https://github.com/baicie/template-repo/archive/refs/heads/master.zip'
-export const DEFAULT_TEMPLATE_SRC_GITEE = 'https://github.com/baicie/template-repo/archive/refs/heads/master.zip'
+export const DEFAULT_TEMPLATE_SRC_GITEE = 'https://gitee.com/white-cie/template-repo.git'
 export const TEMPLATE_CREATOR = 'template_creator.js'
